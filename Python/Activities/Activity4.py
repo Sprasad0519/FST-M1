849cc9cfdccaf6c8e44cedb03fa16ad016365d64
Sprasad0519 <sima.prasad@ibm.com>
2025-01-01 23:00:54 +0530

    Add files via upload

---
+++ Python/Activities/Activity4.py
@@ -0,0 +1,36 @@
+#Get the users names
+user1 = input("What is Player 1's name ?")
+user2 = input("What is Player 2's name ?")
+#Get the users choice
+
+user1_answer = input(user1 + " , do you want to choose rock,paper or scissors ? ").lower()
+user2_answer = input(user2 + ",do you want to choose rock,paper").lower()
+
+if user1_answer == user2_answer :
+    print("It's a tie!")
+elif user1_answer == 'rock' :
+    if user2_answer =='scissors' :
+       print("Rock wins !")
+    else:
+        print("Paper wins !")
+elif user1_answer == 'scissors' :
+    if user2_answer == 'paper' :
+        print("Scissors wins!") 
+    else :
+        print("Rock wins!")
+elif user1_answer == 'paper' :
+    if user2_answer == 'rock' :
+        print("Paper wins !")
+    else :
+        print("Scissors win!")
+else:
+  print("Invalid input!")    
+
+repeat = input("Do you want to play another round? Yes/No :").lower()
+if(repeat == 'Yes'):
+    pass
+elif(repeat == 'no'):
+    raise SystemExit
+else:
+   print("You entered an invalid option.Exiting now.")
+   raise SystemExit
