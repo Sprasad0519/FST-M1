5c4c0c95b4791bafbdd4a1a6a3311995b49ff2dc
Sprasad0519 <sima.prasad@ibm.com>
2025-02-23 00:28:27 +0530

    Add files via upload

--- Cucumber/Activities/TestRunner.java
+++ Cucumber/Activities/TestRunner.java
@@ -24,6 +24,13 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 
 import io.cucumber.junit.platform.engine.Constants;
 
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+
+import io.cucumber.junit.platform.engine.Constants;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("Features")
@@ -31,7 +38,7 @@ import io.cucumber.junit.platform.engine.Constants;
   key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions"
 )
 @ConfigurationParameter(
-  key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity3"
+  key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity4"
 )
 
 public class TestRunner {}
